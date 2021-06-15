5785021fde5adabd7a5dcf91fd5480dfc5fe6d30
mariaost254 <mariaost254@gmail.com>
2021-06-15 15:47:52 +0300

    added header to all pages, updated css files

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ window.addEventListener("beforeunload", function () {
     document.body.classList.add("animate-out");
   });
 
+//email and user registered from login page/link pop up 
+let email = "a@a.com";
+let user = "Temp User";
+
+//executes on calc page load
+function initUser() { //or welcome and logout?
+  document.getElementById("userHeader").innerHTML = "Logged in as "+ user + " ("+email+") " +("<a href=\"login.html\" onclick=\"relogin()\"><span style=\"color:#4D9DE0;font-size:0.8em;font-style:normal;\">Not you?</span></a>");
+}
+
+//relogin from calc page
+function relogin() {
+  //clear all saved data 
+  console.log("test");
+}
+
+
