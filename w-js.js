b362e9ca9abd2a6d7e971d6247172cd2d319b071
mariaost254 <mariaost254@gmail.com>
2021-06-23 09:18:24 +0300

    more front end updates

--- w-js.js
+++
@@ -1,6 +0,0 @@
-$(function() { //change to event listener
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
-  });
